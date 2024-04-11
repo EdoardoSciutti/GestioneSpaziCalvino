60bde2b2f934baaa060a76c414a02c6df1fe0c55
Teghino <francesco.tacchino.2005@calvino.edu.it>
2024-04-11 12:23:31 +0200

    aggiunta rooms in model

--- sequelize/model.js
+++ sequelize/model.js
@@ -1,3 +1,16 @@
 const Sequelize = require('sequelize');
 const sequelize = require('./seq.js');
 
+const Rooms = sequelize.define('rooms', {
+    room_id: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    description: Sequelize.STRING
+},
+{
+    tableName: 'rooms',
+    freezeTableName: true
+});
--- sequelize/seq.js
+++ sequelize/seq.js
@@ -1,12 +1,12 @@
-const Sequelize = require('sequelize');
+const Sequelize = require('sequelize'); 
 
-const sequelize = new Sequelize('calvino_booking', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql', 
+const sequelize = new Sequelize('calvino_booking', 'root', '', { // Nome del database, username e password
+  host: 'localhost', // Indirizzo del server MySQL
+  dialect: 'mysql',  // Tipo di database
   port: 3306, // Porta predefinita di MySQL
   define: {
     timestamps: false, // Opzionale: disabilita la generazione automatica dei timestamp
-    freezeTableName: true,
+    freezeTableName: true, // Opzionale: disabilita la pluralizzazione automatica dei nomi delle tabelle
   },
 });
 
