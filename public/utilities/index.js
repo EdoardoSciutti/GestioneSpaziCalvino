40687a8edfdd891b5df044f31c8eca3beff0c859
Edoardo Sciutti <sciutti05@gmail.com>
2024-04-15 11:28:00 +0200

    Calendario
    Manca gestione eventi

--- public/utilities/index.js
+++ public/utilities/index.js
@@ -1,12 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var calendarEl = document.getElementById('bsb-calendar-1');
-  
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-      initialView: 'dayGridMonth',
-      dateClick: function(info) {
-        alert('Data selezionata ' + info.dateStr)
-      }
-    });
-  
-    calendar.render();
+  var calendarEl = document.getElementById('bsb-calendar-1');
+  var aggiungiEventoEl = document.querySelector('.aggiungi-evento');
+  var giornoEl = document.querySelector('.giorno');
+
+  var calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+    height: 'auto',
+    dateClick: function(info) {
+      aggiungiEventoEl.style.display = 'block';
+      giornoEl.textContent = "Aggiungi un Evento nel giorno " + info.dateStr;
+    }
   });
+
+  calendar.render();
+});
+
+function chiudiAggiungi(){
+  var aggiungiEventoEl = document.querySelector('.aggiungi-evento');
+  aggiungiEventoEl.style.display = 'none';
+}
