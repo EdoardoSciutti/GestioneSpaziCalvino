fa54464660a347db15466254ecb896bb5081ddd3
Edoardo Sciutti <147152536+EdoardoSciutti@users.noreply.github.com>
2024-04-17 09:09:41 +0200

    Register Fatto

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //set di ejs
-app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 //cookie
 const cookieParser = require('cookie-parser');
@@ -18,8 +19,15 @@ app.use('/api/bookings', require('./routes/bookings'));
 app.use('/api/auth', require('./routes/login-signup'));
 
 app.get('/', (req, res) => {
-    res.render('../public/views/index.ejs');
+    res.render('../public/views/index.html');
 })
 
+app.get('/register', (req, res) => {
+    res.render('../public/views/register.html');
+})
+
+app.get('/login', (req, res) => {
+    res.render('../public/views/login.html');
+})
 
 app.listen(3000);
