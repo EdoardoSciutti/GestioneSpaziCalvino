73ef92c3e5abe4945c1d9ffabbde1fcfeea84138
Edoardo Sciutti <147152536+EdoardoSciutti@users.noreply.github.com>
2024-04-17 17:45:26 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -7,19 +7,19 @@ function authenticateToken(req, res, next) {
     const { refresh_token } = req.cookies; // Leggi il refresh token dal cookie
     console.log('token: ', access_token);
     if (access_token == null){
-      res.status(401).json({ success: false, message: 'Token not found' });
-      res.redirect('views/login')
+      // Invia solo una risposta
+      return res.status(401).json({ success: false, message: 'Token not found' });
     } 
     jwt.verify(access_token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
       if (err) {
         console.log(err);
         if (refresh_token == null){
           return res.redirect('/login');
-        }  // Se non c'è un refresh token, restituisci un errore 401
+        }  
         jwt.verify(refresh_token, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
           if (err){
             return res.redirect('/login');
-          }  // Se c'è un errore durante la verifica, restituisci un errore 403
+          }  
           const access_token = jwt.sign({ name: user.name }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '24h' }); // Genera un nuovo access token
           const refresh_token = jwt.sign({ name: user.name }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: '7d' }); // Genera un nuovo refresh token
           res.cookie('access_token', access_token, { httpOnly: true }); // Invia il nuovo access token come cookie
@@ -28,7 +28,7 @@ function authenticateToken(req, res, next) {
           req.user = user;
           next(); // Passa al prossimo middleware
       });
-      } // se c'è un errore durante la verifica, restituisci un errore 403
+      } 
       req.user = user;
       next(); // passa al prossimo middleware
     });
