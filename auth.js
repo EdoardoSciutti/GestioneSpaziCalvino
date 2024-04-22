111e1852daf1447b969daa4f665c4d5b8912cf52
GesualdiS <simone.gesualdi.2004@calvino.edu.it>
2024-04-22 10:24:14 +0200

    Creazione endpoint per google

--- auth.js
+++ auth.js
@@ -1,5 +1,30 @@
+//   +-------------------------------------------------+
+//   |   I require the modules that I'm going to use   |
+//   +-------------------------------------------------+
+
+const passport = require('passport');
+const GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
+
+//   +-------------------------------------------------+
+//   |   I take the variables store in the file .env   |
+//   +-------------------------------------------------+
+
+const googleClientId = process.env.GOOGLE_CLIENT_ID;
+const googleClientSecret = process.env.GOOGLE_CLIENT_SECRET;
+const db = require('./database.js')
+
+//   +-------------------------------------------------+
+//   |   I take the variables store in the file .env   |
+//   +-------------------------------------------------+
+
+const serverHost = process.env.SERVER_HOST;
+const protocol = process.env.SERVER_PROTOCOL;
+
+//   +-------------------------------------------------+
+//   |   We create the functions we are gonna export   |
+//   +-------------------------------------------------+
 
 // verify the token and redirect to login if it is not valid
 function authenticateToken(req, res, next) {
@@ -33,5 +58,27 @@ function authenticateToken(req, res, next) {
       next(); // passa al prossimo middleware
     });
 }
+
+passport.use(new GoogleStrategy(
+  {
+      clientID:     googleClientId,
+      clientSecret: googleClientSecret,
+      //change this path to newest
+      callbackURL: `${protocol}://${serverHost}/api/auth/googleCallback`,
+      passReqToCallback   : true
+  },
+  //this function is called when the user is successfully authenticated
+  (request, accessToken, refreshToken, profile, done) => {
+      // What to do when the user is authenticated
+  }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
 
 module.exports = { authenticateToken};
