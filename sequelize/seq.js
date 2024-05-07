e88cf7056989400be3e83e2b87b0c6e457a2eef4
GesualdiS <simone.gesualdi.2004@calvino.edu.it>
2024-05-07 08:38:11 +0200

    Aggiunto il db online

--- sequelize/seq.js
+++ sequelize/seq.js
@@ -1,13 +1,30 @@
 const Sequelize = require('sequelize'); 
+const dotenv = require('dotenv');
+const fs = require("fs")
+const path = require("path")
+//   +-------------------------------------------------+
+//   |   I take the variables store in the file .env   |
+//   +-------------------------------------------------+
 
-const sequelize = new Sequelize('calvino_booking', 'root', '', { // Nome del database, username e password
-  host: 'localhost', // Indirizzo del server MySQL
-  dialect: 'mysql',  // Tipo di database
-  port: 3306, // Porta predefinita di MySQL
+const database_user = process.env.DATABASE_USER;
+const database_password = process.env.DATABASE_PASSWORD;
+const database_name = process.env.DATABASE_NAME;
+const database_host = process.env.DATABASE_HOST;
+
+const sequelize = new Sequelize(database_name, database_user, database_password, {
+  host: database_host,
+  dialect: 'mysql',
+  port: 3306,
   define: {
-    timestamps: false, // Opzionale: disabilita la generazione automatica dei timestamp
-    freezeTableName: true, // Opzionale: disabilita la pluralizzazione automatica dei nomi delle tabelle
+    timestamps: false,
+    freezeTableName: true,
   },
+  dialectOptions: {
+    ssl: {
+      ca: fs.readFileSync(path.join(__dirname, "../certificate/Microsoft RSA Root Certificate Authority 2017.crt")),
+      rejectUnauthorized: false // Add this line
+    }
+  }
 });
 
 module.exports = sequelize;
