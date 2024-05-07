b7f8d977a579edbb597b5369b911b1043101a472
Edoardo Sciutti <sciutti05@gmail.com>
2024-05-07 09:17:28 +0200

    VERO BACKUP 1.0

--- sequelize/seq.js
+++ sequelize/seq.js
@@ -6,25 +6,15 @@ const path = require("path")
 //   |   I take the variables store in the file .env   |
 //   +-------------------------------------------------+
 
-const database_user = process.env.DATABASE_USER;
-const database_password = process.env.DATABASE_PASSWORD;
-const database_name = process.env.DATABASE_NAME;
-const database_host = process.env.DATABASE_HOST;
 
-const sequelize = new Sequelize(database_name, database_user, database_password, {
-  host: database_host,
+const sequelize = new Sequelize("calvino_booking", "root", "", {
+  host: "localhost",
   dialect: 'mysql',
   port: 3306,
   define: {
     timestamps: false,
     freezeTableName: true,
   },
-  dialectOptions: {
-    ssl: {
-      ca: fs.readFileSync(path.join(__dirname, "../certificate/Microsoft RSA Root Certificate Authority 2017.crt")),
-      rejectUnauthorized: false // Add this line
-    }
-  }
 });
 
 module.exports = sequelize;
