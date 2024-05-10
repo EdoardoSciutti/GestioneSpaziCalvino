095970c7e6f57cc9dc3750247c7e0eee04cf4bc9
Francesco Tacchino <113978741+Teghino@users.noreply.github.com>
2024-05-10 12:54:09 +0200

    ll

--- routes/bookings.js
+++ routes/bookings.js
@@ -100,7 +100,7 @@ router.post('/deleteBooking', authenticateToken, async (req, res) => {
                     booking.destroy();
                     res.status(200).json({ success: true, message: 'Booking deleted' });
                 } else {
-                    res.status(400).json({ success: false, message: 'You are not authorized to delete this booking' });
+                    res.status(401).json({ success: false, message: 'You are not authorized to delete this booking' });
                 }
             });
         } 
